bd0efac84acb283a3a5d3cbe1a4edce8503c6f5e
Harshitha <Harshitha.Raghava2@mindtree.com>
2019-09-06 11:13:04 +0530

    commit

--- App.java
+++ App.java
@@ -1,12 +1,7 @@
-package com.mindtree.hello;
-
-import java.util.logging.Logger;
-
 public class App 
 {
     public static void main( String[] args )
     {
-        Logger logger=Logger.getLogger("com.api.jar");
-        logger.info("helloworld");
+        System.out.println("Helloworld");
     }
 }
