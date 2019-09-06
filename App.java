fd4c52af6cc8214fcd6dc6565ab919656d5704c5
Harshitha <Harshitha.Raghava2@mindtree.com>
2019-09-06 10:03:55 +0530

    second

---
+++ App.java
@@ -0,0 +1,12 @@
+package com.mindtree.hello;
+
+import java.util.logging.Logger;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Logger logger=Logger.getLogger("com.api.jar");
+        logger.info("helloworld");
+    }
+}
